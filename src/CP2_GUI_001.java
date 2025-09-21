17b73873cccd3d728649f973dc045cb0b74f8d94
cops9080 <up93py@gmail.com>
2025-09-21 15:58:41 +0900

    프레임 생성

--- src/CP2_GUI_001.java
+++ src/CP2_GUI_001.java
@@ -1,6 +1,15 @@
+import javax.swing.*;
+
 public class CP2_GUI_001 {
     public static void main(String[] args) {
         //Graphical User Interface, GUI는 컴포넌트들로 구성됨
         // 자바에서 GUI의 종류는 AWT와 SWING이 있다
+
+        //프레임 생성
+        JFrame f = new JFrame("Frame Test");
+        f.setTitle("My Frame");
+        f.setSize(300,200);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
